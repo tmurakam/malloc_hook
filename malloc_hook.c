7aac7754357d118bc6a302c4452e93b24293fbaa
Takuya Murakami <tmurakam@tmurakam.org>
2022-09-25 22:28:10 +0900

    refactor: add comments, cleanup code, etc

--- malloc_hook.c
+++ malloc_hook.c
@@ -43,18 +43,24 @@ static void * (*org_malloc)(size_t) = NULL;
 static void * (*org_realloc)(void *, size_t) = NULL;
 static void (*org_free)(void *) = NULL;
 
-static char static_buffer[256];
-static char *buffer_ptr = static_buffer;
 static bool initializing = false;
 static bool in_hook = false;
 
+// initial memory area used by malloc inside dlsym.
+static char static_buffer[256];
+static char *buffer_ptr = static_buffer;
+
 static long malloc_total = 0;
 
+/**
+ * Memory header
+ */
 typedef struct strMemHeader {
-    unsigned long magic;
-    size_t size;
+    unsigned long magic;  // Magic
+    size_t size;  // allocated memory size (excludes this header)
 } MemHeader;
 
+/** MAGIC number of header */
 static const long MAGIC = 0xdeadbeef;
 
 /**
@@ -63,11 +69,13 @@ static const long MAGIC = 0xdeadbeef;
 __attribute__((constructor))
 static void ma_init() {
     if (org_malloc == NULL && !initializing) {
-        initializing = true;
+        pthread_mutex_lock(&ma_mutex);
+        initializing = true; // recursive guard
         org_malloc = dlsym(RTLD_NEXT, "malloc");
         org_realloc = dlsym(RTLD_NEXT, "realloc");
         org_free = dlsym(RTLD_NEXT, "free");
         initializing = false;
+        pthread_mutex_unlock(&ma_mutex);
     }
 }
 
@@ -118,7 +126,9 @@ void *malloc(size_t size) {
     void *ret;
 
     pthread_mutex_lock(&ma_mutex);
-    ma_init();
+
+    ma_init();  // note: this may call malloc recursively
+
     if (org_malloc != NULL) {
         MemHeader *header = org_malloc(sizeof(MemHeader) + size);
         if (header) {
@@ -133,10 +143,11 @@ void *malloc(size_t size) {
             }
         }
     } else {
-        // called from dlsym
+        // called from initial dlsym
         ret = buffer_ptr;
         buffer_ptr += size;
         if (buffer_ptr > static_buffer + sizeof(static_buffer)) {
+            // oops, no memory.
             exit(2);
         }
     }
@@ -153,6 +164,16 @@ void *calloc(size_t n, size_t size) {
     return ptr;
 }
 
+static bool checkHeader(MemHeader *header) {
+    if (header->magic == MAGIC) {
+        return true;
+    } else {
+        // Never happen, not managed by me...
+        fprintf(stderr, "WARNING: memory header broken: %p\n", header);
+        return false;
+    }
+}
+
 /**
  * replaced realloc
  */
@@ -166,12 +187,10 @@ void *realloc(void *oldPtr, size_t newSize) {
 
     if (oldPtr != NULL) {
         header = oldPtr - sizeof(MemHeader);
-        if (header->magic == MAGIC) {
+        hasHeader = checkHeader(header);
+        if (hasHeader) {
             real_ptr = header;
             oldSize = header->size;
-        } else {
-            // Not malloced by me...
-            hasHeader = false;
         }
     }
 
@@ -202,24 +221,27 @@ void *realloc(void *oldPtr, size_t newSize) {
 void free(void *ptr) {
     if (!ptr) return;
 
-    pthread_mutex_lock(&ma_mutex);
-    if ((char*)ptr < static_buffer || static_buffer + sizeof(static_buffer) <= (char*)ptr) {
-        void *real_ptr = ptr;
-        MemHeader *header = ptr - sizeof(MemHeader);
-        size_t size = 0;
-        if (header->magic == MAGIC) {
-            real_ptr = header;
-            size = header->size;
-        }
-        malloc_total -= size;
-
-        if (free_hook && !in_hook) {
-            in_hook = true;
-            free_hook(ptr, size, __builtin_return_address(0));
-            in_hook = false;
-        }
-        org_free(real_ptr);
+    if (static_buffer <= (char*)ptr && (char *)ptr < static_buffer + sizeof(static_buffer)) {
+        // initial malloc area, do nothing
+        return;
     }
+
+    pthread_mutex_lock(&ma_mutex);
+    void *real_ptr = ptr;
+    MemHeader *header = ptr - sizeof(MemHeader);
+    size_t size = 0;
+    if (checkHeader(header)) {
+        real_ptr = header;
+        size = header->size;
+    }
+    malloc_total -= size;
+
+    if (free_hook && !in_hook) {
+        in_hook = true;
+        free_hook(ptr, size, __builtin_return_address(0));
+        in_hook = false;
+    }
+    org_free(real_ptr);
     pthread_mutex_unlock(&ma_mutex);
 }
 
@@ -227,9 +249,6 @@ long get_malloc_total() {
     return malloc_total;
 }
 
-/**
- * @inherit
- */
 void dump_backtrace(int depth) {
     void *trace[depth];
     int n = backtrace(trace, depth);
